bf969afd797719f35ec3f4a76d76d1d065148c21
Kay Alvaer <mosarwe@yahoo.co.uk>
2022-01-01 16:15:11 +0100

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f71c9afdc6c11e045465262d7491b8cb'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '86373551-258c-4916-a9df-a8f7cd392785'
+};
